5c07f7eef358a8e5b162cd73968f99288640e9fa
Fer Rodriguez <32382866+FeRodriguezAut@users.noreply.github.com>
2025-10-04 17:54:12 -0500

    new file:   index.html 	new file:   notas.js 	new file:   rectangular.js 	new file:   visitas.js

---
+++ notas.js
@@ -0,0 +1,8 @@
+let nota1 = parseFloat(prompt("Ingrese nota 1:"));
+let nota2 = parseFloat(prompt("Ingrese nota 2:"));
+let nota3 = parseFloat(prompt("Ingrese nota 3:"));
+
+let total = nota1 + nota2 + nota3;
+let promedio = total / 3;
+console.log("la nota promedio es: " + promedio);
+alert("la nota promedio es: " + promedio);
---
+++ visitas.js
@@ -0,0 +1,7 @@
+let visitas = 100;
+let visitasadicionales = +25;
+let visitasnuevas = visitas + visitasadicionales;
+let errorvisitas = visitasnuevas-10;
+let totalvisitas = errorvisitas*2;
+alert("El total de visitas es: " + totalvisitas);
+
