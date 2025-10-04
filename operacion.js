73d35998180fc529e36c39b0f088d68c605c0ba4
Fer Rodriguez <32382866+FeRodriguezAut@users.noreply.github.com>
2025-10-04 18:33:30 -0500

    Operaciones 5

---
+++ operacion.js
@@ -0,0 +1,5 @@
+// La expresión completa que proporcionaste
+let resultado = (20+10)/5 + 3*2 - 4*(20+10)/5 + 3*2 - 4*(20+10)/5 + 3*2 - 4;
+
+// Imprimimos el resultado en la consola
+console.log(resultado); // Salida: -28
